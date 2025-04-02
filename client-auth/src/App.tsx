f3f8c55b255ae053a3de6ab2293f8714ba8a96a0
vannguyenv12 <npnv.vn1@gmail.com>
2025-04-02 16:48:06 +0700

    codebase

---
+++ client-auth/src/App.tsx
@@ -0,0 +1,12 @@
+import ForgotPasswordPage from './pages/forgot-password/forgot-password-page';
+import ResetPasswordPage from './pages/reset-password/reset-password-page';
+
+function App() {
+  return (
+    <>
+      <ForgotPasswordPage />
+    </>
+  );
+}
+
+export default App;
